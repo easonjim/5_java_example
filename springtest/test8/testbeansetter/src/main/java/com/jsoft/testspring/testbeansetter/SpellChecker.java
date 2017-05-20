2899be274f25eea4bac392860c4ff5332cc9d004
Jim <easonjim@163.com>
2017-05-20 23:27:44 +0800

    spring　ｔｅｓｔ8 beansetter test

---
+++ springtest/test8/testbeansetter/src/main/java/com/jsoft/testspring/testbeansetter/SpellChecker.java
@@ -0,0 +1,11 @@
+package com.jsoft.testspring.testbeansetter;
+
+public class SpellChecker {
+	public SpellChecker(){
+		System.out.println("SpellChecker无参数构造函数初始化");
+	}
+	
+	public void checkSpelling(){
+		System.out.println("SpellChecker检查方法");
+	}
+}
