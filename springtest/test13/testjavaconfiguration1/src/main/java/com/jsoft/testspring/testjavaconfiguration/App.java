63b6fe108d5dcfdcfb3445116260715a562a3505
Jim <easonjim@163.com>
2017-05-24 21:40:37 +0800

    spring test13 javaconfiguration test

---
+++ springtest/test13/testjavaconfiguration1/src/main/java/com/jsoft/testspring/testjavaconfiguration/App.java
@@ -0,0 +1,18 @@
+package com.jsoft.testspring.testjavaconfiguration;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(HelloWorldConfig.class);
+        HelloWorld helloWorld = applicationContext.getBean(HelloWorld.class);
+        helloWorld.getMessage();
+    }
+}
