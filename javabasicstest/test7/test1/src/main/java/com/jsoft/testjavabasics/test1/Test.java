8a3df033fec9d2be5cffc32a723a0c5f57a51066
Jim <easonjim@163.com>
2017-05-31 14:12:52 +0800

    javabasics test7

---
+++ javabasicstest/test7/test1/src/main/java/com/jsoft/testjavabasics/test1/Test.java
@@ -0,0 +1,10 @@
+package com.jsoft.testjavabasics.test1;
+
+public class Test {
+
+	public static void main(String args[]) {
+		Integer x = 5; // boxes int to an Integer object
+		x = x + 10; // unboxes the Integer to a int
+		System.out.println(x);
+	}
+}
