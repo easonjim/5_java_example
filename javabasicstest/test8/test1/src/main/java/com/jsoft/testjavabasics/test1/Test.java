df66498e0189a57c75918a4f44b18f5cf821591c
Jim <easonjim@163.com>
2017-05-31 14:31:19 +0800

    javabasics test8

---
+++ javabasicstest/test8/test1/src/main/java/com/jsoft/testjavabasics/test1/Test.java
@@ -0,0 +1,11 @@
+package com.jsoft.testjavabasics.test1;
+
+public class Test {
+
+	public static void main(String args[]) {
+		Character a = 'a';
+		char c = new Character('a');
+		
+		System.out.println("She said \"Hello!\" to me.");
+	}
+}
