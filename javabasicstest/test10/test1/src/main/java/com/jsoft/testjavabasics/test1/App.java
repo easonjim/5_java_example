2be4544f11f56cfa9d205fcc21cd13fa87a91272
Jim <easonjim@163.com>
2017-05-31 15:14:00 +0800

    javabasics test10

---
+++ javabasicstest/test10/test1/src/main/java/com/jsoft/testjavabasics/test1/App.java
@@ -0,0 +1,13 @@
+package com.jsoft.testjavabasics.test1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	double[] myList = new double[10];
+    }
+}
---
+++ javabasicstest/test10/test1/src/main/java/com/jsoft/testjavabasics/test1/TestArray1.java
@@ -0,0 +1,13 @@
+package com.jsoft.testjavabasics.test1;
+
+public class TestArray1 {
+
+	public static void main(String[] args) {
+		double[] myList = { 1.9, 2.9, 3.4, 3.5 };
+
+		// Print all the array elements
+		for (double element : myList) {
+			System.out.println(element);
+		}
+	}
+}
