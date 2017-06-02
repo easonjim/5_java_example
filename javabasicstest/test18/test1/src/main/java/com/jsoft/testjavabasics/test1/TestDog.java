20dfdef65a39f9b027c80c2d9ee9fdd1174795ef
Jim <easonjim@163.com>
2017-06-03 01:41:46 +0800

    javabasics test18

---
+++ javabasicstest/test18/test1/src/main/java/com/jsoft/testjavabasics/test1/TestDog.java
@@ -0,0 +1,27 @@
+package com.jsoft.testjavabasics.test1;
+
+class Animal {
+
+	public void move() {
+		System.out.println("Animals can move");
+	}
+}
+
+class Dog extends Animal {
+
+	public void move() {
+		System.out.println("Dogs can walk and run");
+	}
+}
+
+public class TestDog {
+
+	public static void main(String args[]) {
+		Animal a = new Animal(); // Animal reference and object
+		Animal b = new Dog(); // Animal reference but Dog object
+
+		a.move();// runs the method in Animal class
+
+		b.move();// Runs the method in Dog class
+	}
+}
---
+++ javabasicstest/test18/test3/src/main/java/com/jsoft/testjavabasics/test3/TestDog.java
@@ -0,0 +1,26 @@
+package com.jsoft.testjavabasics.test3;
+
+class Animal {
+
+	public void move() {
+		System.out.println("Animals can move");
+	}
+}
+
+class Dog extends Animal {
+
+	public void move() {
+		super.move(); // invokes the super class method
+		System.out.println("Dogs can walk and run");
+	}
+}
+
+public class TestDog {
+
+	public static void main(String args[]) {
+
+		Animal b = new Dog(); // Animal reference but Dog object
+		b.move(); // Runs the method in Dog class
+
+	}
+}
