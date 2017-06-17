01944e8209bc18d8629a61faa90995166b259826
Jim <easonjim@163.com>
2017-06-18 02:41:23 +0800

    mavenrepository local test

---
+++ mavenrepository/usetestcommon/src/main/java/com/jsoft/usetestcommon/App.java
@@ -0,0 +1,15 @@
+package com.jsoft.usetestcommon;
+
+import com.jsoft.testcommon.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Common.test();
+    }
+}
