0c70705659b2d908706aa8ee4dac134919aca498
easonjim <easonjim@163.com>
2017-09-01 21:03:03 +0800

    tutorialspoint sprintmvc test7

---
+++ springmvc/tutorialspoint/test7/test1/src/main/java/com/jsoft/testspringmvc/model/User.java
@@ -0,0 +1,41 @@
+package com.jsoft.testspringmvc.model;
+
+public class User {
+
+	private String username;
+	private String password;
+	private String address;
+	private boolean receivePaper;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public boolean isReceivePaper() {
+		return receivePaper;
+	}
+
+	public void setReceivePaper(boolean receivePaper) {
+		this.receivePaper = receivePaper;
+	}
+}
