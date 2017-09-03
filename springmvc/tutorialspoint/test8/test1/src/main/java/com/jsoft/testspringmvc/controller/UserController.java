3ad466804146adf2753b0bdf76385e78e06f199b
easonjim <easonjim@163.com>
2017-09-03 21:27:32 +0800

    tutorialspoint springmvc test8

---
+++ springmvc/tutorialspoint/test8/test1/src/main/java/com/jsoft/testspringmvc/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.jsoft.testspringmvc.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.jsoft.testspringmvc.model.User;
+
+import org.springframework.ui.ModelMap;
+
+@Controller
+public class UserController {
+
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	public ModelAndView user() {
+		User user = new User();
+		user.setFavoriteFrameworks((new String[] { "Spring MVC", "Struts 2" }));
+		ModelAndView modelAndView = new ModelAndView("user", "command", user);
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "/addUser", method = RequestMethod.POST)
+	public String addUser(@ModelAttribute("SpringWeb") User user, ModelMap model) {
+		model.addAttribute("username", user.getUsername());
+		model.addAttribute("password", user.getPassword());
+		model.addAttribute("address", user.getAddress());
+		model.addAttribute("receivePaper", user.isReceivePaper());
+		model.addAttribute("favoriteFrameworks", user.getFavoriteFrameworks());
+		return "users";
+	}
+
+	@ModelAttribute("webFrameworkList")
+	public List<String> getWebFrameworkList() {
+		List<String> webFrameworkList = new ArrayList<String>();
+		webFrameworkList.add("Spring MVC");
+		webFrameworkList.add("Struts 1");
+		webFrameworkList.add("Struts 2");
+		webFrameworkList.add("Apache Wicket");
+		return webFrameworkList;
+	}
+}
---
+++ springmvc/tutorialspoint/test8/test1/src/main/java/com/jsoft/testspringmvc/model/User.java
@@ -0,0 +1,50 @@
+package com.jsoft.testspringmvc.model;
+
+public class User {
+
+	private String username;
+	private String password;
+	private String address;
+	private boolean receivePaper;
+	private String[] favoriteFrameworks;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public boolean isReceivePaper() {
+		return receivePaper;
+	}
+
+	public void setReceivePaper(boolean receivePaper) {
+		this.receivePaper = receivePaper;
+	}
+
+	public String[] getFavoriteFrameworks() {
+		return favoriteFrameworks;
+	}
+
+	public void setFavoriteFrameworks(String[] favoriteFrameworks) {
+		this.favoriteFrameworks = favoriteFrameworks;
+	}
+}
