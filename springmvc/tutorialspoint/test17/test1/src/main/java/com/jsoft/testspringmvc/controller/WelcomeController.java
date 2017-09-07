fc934bff0a018e919f6d2e9b23cb7a2306a40fc3
easonjim <easonjim@163.com>
2017-09-07 08:25:48 +0800

    tutorialspoint springmvc test17

---
+++ springmvc/tutorialspoint/test17/test1/src/main/java/com/jsoft/testspringmvc/controller/WelcomeController.java
@@ -0,0 +1,18 @@
+package com.jsoft.testspringmvc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class WelcomeController extends AbstractController {
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		ModelAndView model = new ModelAndView("welcome");
+		model.addObject("message", "Welcome!");
+		return model;
+	}
+}
