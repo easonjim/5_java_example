d10a80ef717110b356e539d70af215bd46c5144f
Jim <easonjim@163.com>
2017-09-08 00:51:43 +0800

    tutorialspoint springmvc test19

---
+++ springmvc/tutorialspoint/test19/test1/src/main/java/com/jsoft/testspringmvc/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.jsoft.testspringmvc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+public class UserController extends MultiActionController {
+
+	public ModelAndView home(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView model = new ModelAndView("home");
+		model.addObject("message", "Home");
+		return model;
+	}
+
+	public ModelAndView add(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView model = new ModelAndView("user");
+		model.addObject("message", "Add");
+		return model;
+	}
+
+	public ModelAndView remove(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView model = new ModelAndView("user");
+		model.addObject("message", "Remove");
+		return model;
+	}
+}
---
+++ springmvc/tutorialspoint/test19/test1/src/main/webapp/WEB-INF/jsp/home.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Home</title>
+</head>
+<body>
+<body>
+<a href="user/add.htm" >Add</a> <br>
+<a href="user/remove.htm" >Remove</a>
+</body>
+</html>l>
