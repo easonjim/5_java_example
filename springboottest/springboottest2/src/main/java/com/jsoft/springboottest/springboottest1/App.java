7234495010b7a2c6056b3860704863fe3ee4f9a3
Jim <jim@JimdeMBP.lan>
2017-09-13 23:41:04 +0800

    springboot swagger2 test

---
+++ springboottest/springboottest2/src/main/java/com/jsoft/springboottest/springboottest1/App.java
@@ -0,0 +1,17 @@
+package com.jsoft.springboottest.springboottest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ springboottest/springboottest2/src/main/java/com/jsoft/springboottest/springboottest1/controller/User.java
@@ -0,0 +1,25 @@
+package com.jsoft.springboottest.springboottest1.controller;
+
+public class User {
+	private long id;
+	private String name;
+	private int age;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
