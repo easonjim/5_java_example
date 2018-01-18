53da56e6dbbe6a5516b7aa80dc4ef02c3fb29dc6
Jim <easonjim@163.com>
2018-01-18 08:32:47 +0800

    增加多模块测试实例

---
+++ maventest/test10/testproject/app-desktop-ui/src/main/java/com/jsoft/test/App.java
@@ -0,0 +1,14 @@
+package com.jsoft.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Test.test();
+    }
+}
---
+++ maventest/test10/testproject/bus-core-api/src/main/java/com/jsoft/test/Test.java
@@ -0,0 +1,13 @@
+package com.jsoft.test;
+
+/**
+ * This is Description
+ *
+ * @author jim
+ * @date 2018/01/16
+ */
+public class Test {
+    public static void test(){
+        System.out.println("test");
+    }
+}
